ac9fbef51dd9b75d7c453e1c0a42f89f5a855cdd
Дмитрий Кречетников <dkrech07@gmail.com>
2021-06-16 13:34:36 +0500

    Выполняет геокодирование

--- ymap.php
+++ ymap.php
@@ -1,33 +1,48 @@
 <div id="map" style="width: 100%; height:500px"></div>
 
 <script src="https://api-maps.yandex.ru/2.1/?apikey=4f3c09de-626b-498a-bc29-cff656b39532&lang=ru_RU" type="text/javascript"></script>
+<script src="direct_geocode.js" type="text/javascript"></script>
+
 <script type="text/javascript">
 ymaps.ready(init);
-function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [<?php echo $list[0]['point']; ?>],
-        zoom: 16
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
- 
-    var myCollection = new ymaps.GeoObjectCollection(); 
 
-    <?php foreach ($list as $row): ?>
-    var myPlacemark = new ymaps.Placemark([
-        <?php echo $row['point']; ?>
-    ], {
-        balloonContent: '<?php echo $row['name']; ?>'
-    }, {
-        preset: 'islands#icon',
-        iconColor: '#0000ff'
+function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [55.753994, 37.622093],
+        zoom: 9
     });
-    myCollection.add(myPlacemark);
-    <?php endforeach; ?>
- 
-    myMap.geoObjects.add(myCollection);
-    
-    // Сделаем у карты автомасштаб, чтобы были видны все метки;
-    myMap.setBounds(myCollection.getBounds(),{checkZoomRange:true, zoomMargin:9});
+
+    // Поиск координат переданного адреса;
+    ymaps.geocode('Екатеринбург, Решетникова 4', {
+        /**
+         * Опции запроса
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/geocode.xml
+         */
+        // Сортировка результатов от центра окна карты.
+        // boundedBy: myMap.getBounds(),
+        // strictBounds: true,
+        // Вместе с опцией boundedBy будет искать строго внутри области, указанной в boundedBy.
+        // Если нужен только один результат, экономим трафик пользователей.
+        results: 1
+    }).then(function (res) {
+            // Выбираем первый результат геокодирования.
+            var firstGeoObject = res.geoObjects.get(0),
+                // Координаты геообъекта.
+                coords = firstGeoObject.geometry.getCoordinates(),
+                // Область видимости геообъекта.
+                bounds = firstGeoObject.properties.get('boundedBy');
+
+            firstGeoObject.options.set('preset', 'islands#darkBlueDotIconWithCaption');
+            // Получаем строку с адресом и выводим в иконке геообъекта.
+            firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
+
+            // Добавляем первый найденный геообъект на карту.
+            myMap.geoObjects.add(firstGeoObject);
+            // Масштабируем карту на область видимости геообъекта.
+            myMap.setBounds(bounds, {
+                // Проверяем наличие тайлов на данном масштабе.
+                checkZoomRange: true
+            });
+        });
 }
 </script>
