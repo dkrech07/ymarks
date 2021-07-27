95af9e2d21fe35fbe3c060e093a6965fe8111dc4
Дмитрий Кречетников <dkrech07@gmail.com>
2021-07-27 12:41:38 +0500

    Вносит правки

--- templates/main.php
+++ templates/main.php
@@ -13,42 +13,42 @@
 
 <body>
     <div class="popup-wrapper"></div>
-    <div class="wrapper">
-        <header class="main-section main-header">
-            <b class="index-logo">Where is my customs?</b>
-        </header>
 
-        <main>
-            <div id="map" style="width: 100%; height: 800px"></div>
-            <span class='customs-count'>Количество постов: </span><span class='customs-count-number'></span>
+    <header class="main-header wrapper">
+        <b class="index-logo">Where is my customs?</b>
+    </header>
 
-            <form class="customs-form" action="" method="post">
-                <label for="main">Все посты</label>
-                <input type="checkbox" id="main" name="main" value="" checked>
-                <label for="head">Головные таможни</label>
-                <input type="checkbox" id="head" name="head" value="">
-                <label for="excise">Посты акцизной таможни</label>
-                <input type="checkbox" id="excise" name="excise" value="">
-                <label for="others">Прочие посты</label>
-                <input type="checkbox" id="others" name="others" value="">
-                <label class="table" for="table">Показать таблицу</label>
-                <input type="checkbox" id="table" name="table" value="">
-            </form>
-        </main>
-    </div>
+    <main class="wrapper">
+        <div id="map"></div>
+
+        <span class='customs-count'>Количество постов: </span><span class='customs-count-number'></span>
+
+        <form class="customs-form" action="" method="post">
+            <label for="main">Все посты</label>
+            <input type="checkbox" id="main" name="main" value="" checked>
+            <label for="head">Головные таможни</label>
+            <input type="checkbox" id="head" name="head" value="">
+            <label for="excise">Посты акцизной таможни</label>
+            <input type="checkbox" id="excise" name="excise" value="">
+            <label for="others">Прочие посты</label>
+            <input type="checkbox" id="others" name="others" value="">
+            <label class="table" for="table">Показать таблицу</label>
+            <input type="checkbox" id="table" name="table" value="">
+        </form>
+    </main>
+
     <div id="table-footer"></div>
-    <div class="wrapper">
-        <footer class="footer">
-          <span class="version-title">v. 0.7 alpha</span>
-        </footer>
-    </div>
+
+    <footer class="footer">
+         <span class="version-title">v. 0.7 alpha</span>
+    </footer>
+
     <script type="text/javascript">
         var obj = <?php echo json_encode($list, JSON_UNESCAPED_UNICODE); ?>
     </script>
 
     <script src="../js/script.js"></script>
     <script src="../js/ymap.js"></script>
-
 </body>
 
 </html>
